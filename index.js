3093d66710c7fd7a907b4183f5dbd008f991e3ad
maynut <libes1234@naver.com>
2022-01-11 11:53:17 +0900

    ++

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-// window.addEventListener("wheel", function (e) {
-//   e.preventDefault();
-// }, { passive: false });
+const introPage = document.querySelector('.page-intro');
+const introLogo = document.querySelector('.intro-logo');
+let page = 0;
 
-window.onload = () => {
-  document.querySelector('.intro-logo').style.opacity = 1;
-  document.querySelector('.intro-logo').style.marginTop = 0;
-  setTimeout(() => {
-    document.querySelector('.top-nav').style.opacity = 1;
-  }, 1500);
-}
+introLogo.style.opacity = 1;
+introLogo.style.marginTop = 0;
+
+introLogo.addEventListener('click', () => {
+  if (page === 0) {
+    introPage.style.width = 0;
+    // introPage.style.left = '50%';
+  
+    introLogo.style.transform = 'none';
+    introLogo.style.top = '22px';
+    introLogo.style.left = '26px';
+    introLogo.style.width = '150px';
+
+    page = 1;
+  } else {
+    introPage.style = '';
+
+    introLogo.style = '';
+    introLogo.style.opacity = 1;
+    introLogo.style.marginTop = 0;
+
+    page = 0;
+  }
+});
